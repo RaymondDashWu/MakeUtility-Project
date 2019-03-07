6771d56cc73341b44cdbe03abf79c0316f503954
RaymondDashWu <raymond31670@hotmail.com>
2019-03-06 23:51:07 -0800

    [ADD] SVGo implementation

--- main.go
+++ main.go
@@ -1,73 +1,89 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
+	"math/rand"
 	"os"
+	"time"
+
+	svg "github.com/ajstarks/svgo"
 )
+
+func rn(n int) int { return rand.Intn(n) }
 
 func main() {
 	data := []int{25, 25, 100}
+	_ = data
+	canvas := svg.New(os.Stdout)
+	width := 500
+	height := 500
+	nstars := 250
+	style := "font-size:48pt;fill:white;text-anchor:middle"
 
-	w, h := len(data)*60+10, 100
-	r := image.Rect(0, 0, w, h)
-	img := image.NewRGBA(r)
-	bg := image.NewUniform(color.RGBA{240, 240, 240, 255})
-	// blue := image.NewUniform(color.RGBA{250, 180, 180, 255})
+	rand.Seed(time.Now().Unix())
+	canvas.Start(width, height)
+	canvas.Rect(0, 0, width, height)
+	for i := 0; i < nstars; i++ {
+		canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
+	}
+	canvas.Circle(width/2, height, width/2, "fill:rgb(77, 117, 232)")
+	// Note: Commented out for SVGo version
+	// w, h := len(data)*60+10, 100
+	// r := image.Rect(0, 0, w, h)
+	// img := image.NewRGBA(r)
+	// bg := image.NewUniform(color.RGBA{240, 240, 240, 255})
+	// // blue := image.NewUniform(color.RGBA{250, 180, 180, 255})
 
-	draw.Draw(img, r, bg, image.Point{0, 0}, draw.Src)
+	// draw.Draw(img, r, bg, image.Point{0, 0}, draw.Src)
 
+	// // for y := 0; y < h; y++ {
+	// // 	for x := 0; x < w; x++ {
+	// // 		img.Set(x, y, color.RGBA{255, 255, 255, 255})
+	// // 	}
+	// // }
+
+	// mask := image.NewRGBA(image.Rect(0, 0, w, h))
 	// for y := 0; y < h; y++ {
 	// 	for x := 0; x < w; x++ {
-	// 		img.Set(x, y, color.RGBA{255, 255, 255, 255})
+	// 		alpha := uint8(0)
+	// 		switch {
+	// 		case y < 30:
+	// 			alpha = 255
+	// 		case y < 50:
+	// 			alpha = 100
+	// 		}
+	// 		mask.Set(x, y, color.RGBA{
+	// 			R: uint8((x + y) & 255),
+	// 			G: uint8((x + y) << 1 & 255),
+	// 			B: uint8((x + y) << 2 & 255),
+	// 			A: alpha,
+	// 		})
 	// 	}
 	// }
 
-	mask := image.NewRGBA(image.Rect(0, 0, w, h))
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			alpha := uint8(0)
-			switch {
-			case y < 30:
-				alpha = 255
-			case y < 50:
-				alpha = 100
-			}
-			mask.Set(x, y, color.RGBA{
-				R: uint8((x + y) & 255),
-				G: uint8((x + y) << 1 & 255),
-				B: uint8((x + y) << 2 & 255),
-				A: alpha,
-			})
-		}
-	}
+	// for i, dp := range data {
+	// 	x0, y0 := (i*60 + 10), 100-dp
+	// 	x1, y1 := (i+1)*60-1, 100
+	// 	bar := image.Rect(x0, y0, x1, y1)
+	// 	grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
+	// 	draw.Draw(img, bar, grey, image.Point{0, 0}, draw.Src)
 
-	for i, dp := range data {
-		x0, y0 := (i*60 + 10), 100-dp
-		x1, y1 := (i+1)*60-1, 100
-		bar := image.Rect(x0, y0, x1, y1)
-		grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
-		draw.Draw(img, bar, grey, image.Point{0, 0}, draw.Src)
+	// 	red := image.NewUniform(color.RGBA{255, 0, 0, 255})
+	// 	draw.DrawMask(img, bar, red, image.Point{0, 0}, mask, image.Point{x0, y0}, draw.Over)
 
-		red := image.NewUniform(color.RGBA{255, 0, 0, 255})
-		draw.DrawMask(img, bar, red, image.Point{0, 0}, mask, image.Point{x0, y0}, draw.Over)
+	// 	// for x := i*60 + 10; x < (i+1)*60; x++ {
+	// 	// 	for y := 100; y >= (100 - dp); y-- {
+	// 	// 		img.Set(x, y, color.RGBA{180, 180, 250, 255})
+	// 	// 	}
+	// 	// }
+	// }
 
-		// for x := i*60 + 10; x < (i+1)*60; x++ {
-		// 	for y := 100; y >= (100 - dp); y-- {
-		// 		img.Set(x, y, color.RGBA{180, 180, 250, 255})
-		// 	}
-		// }
-	}
-
-	f, err := os.Create("image.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	err = png.Encode(f, img)
-	if err != nil {
-		panic(err)
-	}
+	// f, err := os.Create("image.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+	// err = png.Encode(f, img)
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
