ae4a3566fbff336f2eaf4a177506180a19b05927
RaymondDashWu <raymond31670@hotmail.com>
2019-03-07 11:04:40 -0800

    [ADD] SVG + max height

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"math/rand"
 	"os"
-	"time"
 
 	svg "github.com/ajstarks/svgo"
 )
@@ -11,27 +10,44 @@ import (
 func rn(n int) int { return rand.Intn(n) }
 
 func main() {
-	f, err := os.OpenFile("demo.svg", os.O_RDWR|os.O_CREATE, 0755)
-	if err != nil {
-		panic(err)
+	data := []struct {
+		Month string
+		Usage int
+	}{
+		{"Jan", 171},
+		{"Feb", 180},
+		{"Mar", 100},
+		{"Apr", 87},
+		{"May", 66},
+		{"Jun", 40},
+		{"Jul", 30},
+		{"Aug", 55},
 	}
-	defer f.Close()
 
-	// data := []int{25, 25, 100}
-	canvas := svg.New(f)
-	width := 500
-	height := 500
-	nstars := 250
-	style := "font-size:48pt;fill:white;text-anchor:middle"
-
-	rand.Seed(time.Now().Unix())
+	// []int{25, 25, 100}
+	canvas := svg.New(os.Stdout)
+	width := len(data)*60 + 10
+	max := 0
+	height := 300
+	for _, item := range data {
+		if item.Usage > max {
+			max = item.Usage
+		}
+	}
 	canvas.Start(width, height)
-	canvas.Rect(0, 0, width, height)
-	for i := 0; i < nstars; i++ {
-		canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
+	for i, val := range data {
+		percent := val.Usage * height / max
+		canvas.Rect(i*60+10, height-percent, 50, percent, "fill:rgb(77,200,232)")
 	}
-	canvas.Circle(width/2, height, width/2, "fill:rgb(77, 117, 232)")
-	canvas.Text(width/2, height*4/5, "hello, world", style)
+
+	// rand.Seed(time.Now().Unix())
+	// canvas.Start(width, height)
+	// canvas.Rect(0, 0, width, height)
+	// for i := 0; i < nstars; i++ {
+	// 	canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
+	// }
+	// canvas.Circle(width/2, height, width/2, "fill:rgb(77, 117, 232)")
+	// canvas.Text(width/2, height*4/5, "hello, world", style)
 	canvas.End()
 }
 
