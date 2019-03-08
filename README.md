c66a49992069d2e2d3d71b6df9b7f72e09b4eb6d
RaymondDashWu <raymond31670@hotmail.com>
2019-03-08 00:13:30 -0800

    [ADD] README changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# SVG Graphs Using Go!
+
 [![Go Report Card](https://goreportcard.com/badge/github.com/RaymondDashWu/MakeUtility-Project)](https://goreportcard.com/report/github.com/RaymondDashWu/MakeUtility-Project)
 
 Used to provide basic graphs for my latest NLP project.
@@ -8,3 +10,16 @@ go run main.go > demo.svg
 
 Based on Gophercises tutorial
 https://gophercises.com/exercises/image
+
+
+### Tech
+This package makes extensive use of the SVGo library from ajstarks 
+https://github.com/ajstarks/svgo
+
+### Installation
+1. To install this first make sure that your GOPATH is properly set.
+2. Git clone the repo and make changes to the values in the data struct.
+3. Type the following into the directory's terminal
+```sh
+$ go run main.go > FILENAME.svg
+```
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // Based on Gophercises tutorial
 // https://gophercises.com/exercises/image
 
-package main
+package graphGenerator
 
 import (
 	"math/rand"
@@ -11,6 +11,16 @@ import (
 )
 
 func rn(n int) int { return rand.Intn(n) }
+
+// TODO: make functions for image
+// TODO: Godocs: write notes on top of functions and then
+// copy/paste github url into godocs
+// TODO: README
+
+// GenerateSVG - generates an SVG graph based on inputs given.
+// func GenerateSVG() {
+
+// }
 
 func main() {
 	data := []struct {
@@ -42,14 +52,6 @@ func main() {
 
 	}
 
-	// rand.Seed(time.Now().Unix())
-	// canvas.Start(width, height)
-	// canvas.Rect(0, 0, width, height)
-	// for i := 0; i < nstars; i++ {
-	// 	canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
-	// }
-	// canvas.Circle(width/2, height, width/2, "fill:rgb(77, 117, 232)")
-	// canvas.Text(width/2, height*4/5, "hello, world", style)
 	canvas.End()
 }
 
